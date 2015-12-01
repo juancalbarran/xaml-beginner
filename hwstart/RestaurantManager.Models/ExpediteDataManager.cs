b233a0d825560cc188d7797d57b09c42cfeee7c1
juan.albarran <tazjc2505@gmail.com>
2015-11-30 19:40:05 -0800

    INotifyPropertyChanged implementation
    INotifyPropertyChanged implementation to Homework 1

--- hwstart/RestaurantManager.Models/ExpediteDataManager.cs
+++ hwstart/RestaurantManager.Models/ExpediteDataManager.cs
@@ -1,17 +1,31 @@
 ﻿using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 
 namespace RestaurantManager.Models
 {
     public class ExpediteDataManager : DataManager
     {
+        private List<Order> orderItems = new List<Order>();
+
         protected override void OnDataLoaded()
         {
-
+            this.OrderItems = base.Repository.Orders;
         }
 
         public List<Order> OrderItems
         {
-            get { return base.Repository.Orders; }
+            get
+            {
+                return this.orderItems;
+            }
+            set
+            {
+                if (value != this.orderItems)
+                {
+                    this.orderItems = value;
+                    OnPropertyChanged();
+                }
+            }
         }
     }
 }
--- hwstart/RestaurantManager.Models/OrderDataManager.cs
+++ hwstart/RestaurantManager.Models/OrderDataManager.cs
@@ -3,7 +3,10 @@
 namespace RestaurantManager.Models
 {
     public class OrderDataManager : DataManager
-    {       
+    {
+        private List<MenuItem> menuItems = new List<MenuItem>();
+        private List<MenuItem> currentlySelectedMenuItems = new List<MenuItem>();
+
         protected override void OnDataLoaded()
         {
             this.MenuItems = base.Repository.StandardMenuItems;
@@ -15,8 +18,36 @@ namespace RestaurantManager.Models
             };
         }
 
-        public List<MenuItem> MenuItems { get; set; }
+        public List<MenuItem> MenuItems
+        {
+            get
+            {
+                return this.menuItems;
+            }
+            set
+            {
+                if (value != this.menuItems)
+                {
+                    this.menuItems = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
 
-        public List<MenuItem> CurrentlySelectedMenuItems { get; set; }
+        public List<MenuItem> CurrentlySelectedMenuItems
+        {
+            get
+            {
+                return this.currentlySelectedMenuItems;
+            }
+            set
+            {
+                if (value != this.currentlySelectedMenuItems)
+                {
+                    this.currentlySelectedMenuItems = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
     }
 }
