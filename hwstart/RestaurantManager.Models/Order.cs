a805adf2aa289df8b746f9488719ba53223b34ff
juan.albarran <tazjc2505@gmail.com>
2015-12-21 09:16:16 -0800

    DataTemplate and Styles Implementation
    Implementation of DataTemplate and Style to Homework 2

--- hwstart/RestaurantManager.Models/Order.cs
+++ hwstart/RestaurantManager.Models/Order.cs
@@ -20,9 +20,14 @@ namespace RestaurantManager.Models
 
         public bool Expedite { get; set; }
 
+        //public override string ToString()
+        //{
+        //    return String.Join(", ", Items.Select(i => i.Title));
+        //}
+
         public override string ToString()
         {
-            return String.Join(", ", Items.Select(i => i.Title));
+            return Items.Select(i => i.Title).FirstOrDefault().ToString();
         }
     }
 }
--- hwstart/RestaurantManager.Models/Table.cs
+++ hwstart/RestaurantManager.Models/Table.cs
@@ -8,5 +8,10 @@ namespace RestaurantManager.Models
         public int Id { get; set; }
 
         public string Description { get; set; }
+
+        public override string ToString()
+        {
+            return Description;
+        }
     }
 }
